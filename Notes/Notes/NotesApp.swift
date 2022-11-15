67eb3002cb018237fd2063b1ee9d49e951b7b281
Guest User <Guest@Mac-mini-4.local>
2022-11-15 10:36:57 +0100

    initial

---
+++ Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Guest User on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Notes/Notes/TopicView.swift
@@ -0,0 +1,20 @@
+//
+//  TopicView.swift
+//  Notes
+//
+//  Created by Guest User on 15/11/2022.
+//
+
+import SwiftUI
+
+struct TopicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopicView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopicView()
+    }
+}
